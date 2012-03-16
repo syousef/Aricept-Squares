781bd249f83713887c87d4b09ffb419957f463ae
Janos Botyanszki <janosb@berkeley.edu>
2012-03-16 00:07:46 -0700

    routine upload

--- run_single_file.py
+++ run_single_file.py
@@ -1,4 +1,11 @@
-fname = 'data/allsubj/encoding_grp2.txt'
-from aricept_squares_util import *
-all_trials = parse_file(fname)
-plot_em_up(all_trials,fname)
+import sys
+
+if __name__=="__main__":
+	try:
+		fname = sys.argv[1]
+	except IndexError:
+		fname = 'data/allsubj/encoding_grp2.txt'
+
+	from aricept_squares_util import *
+	all_trials = parse_file(fname)
+	plot_em_up(all_trials,fname)
